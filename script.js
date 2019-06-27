559d5f1506033d3062176d7b1b007409368b5578
spiritina <50525685+spiritina@users.noreply.github.com>
2019-06-27 11:51:33 +0300

    unsolved fixed

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 let arr = [],
     fieldModel = [[], [], [], []],
     field = document.getElementById('field'),
-    zero = [];
+    zero = [3,3], 
+    final = [[1,2,3,4],[5,6,7,8], [9,10,11,12], [13,14,15,0]];
 
 generateRandomArray();
-console.log(arr);
-buildFieldModel();
+if(isSolvable(arr)){
+console.log(arr);  
+buildFieldModel();} else {
+    let tmp = arr[0];
+    arr[0] = arr[1];
+    arr[1] = tmp;
+    buildFieldModel();
+    
+}
 
 function generateRandomArray() {
-    for (let i = 0; i < 16; i++) {
-        arr[i] = i;
+    for (let i = 0; i < 15; i++) {
+        arr[i] = i+1;
     }
-    arr.sort(randomizeArray);
+    arr.sort(randomizeArray).push(0);
 }
 
 
@@ -19,15 +27,20 @@ function buildFieldModel() {
     for (let i = 0; i < 4; i++) {
         for (let j = 0; j < 4; j++) {
             fieldModel[j].push(arr[i + (j * 4)]);
-            drawNumber(arr[i + j * 4], i, j);
-            if(arr[i + j * 4]==0){zero = [i,j]
-        }
+            if(i==3&&j==3){drawZero(i,j)}else{drawNumber(arr[i + j * 4], i, j);}
     }}
     console.log(fieldModel);
 }
 
 function randomizeArray(a, b) {
     return Math.random() - 0.5;
+}
+function drawZero(x,y){
+    let div = document.createElement('div');
+    div.classList.add('number');
+    div.classList.add('zero');
+    setPositionOfNumber(div, x,y);
+    field.appendChild(div);
 }
 
 function drawNumber(i, x, y) {
@@ -45,7 +58,7 @@ function drawNumber(i, x, y) {
         console.log(zero);
         if ((Math.abs(currentCoordinates[0]-zero[0])+Math.abs(currentCoordinates[1]-zero[1]))==1){
             changeElementWithZero(this, currentCoordinates);
-            isGameEnded()
+            isGameEnded();
             }
     })
     field.appendChild(div);
@@ -64,10 +77,24 @@ let zeroDiv = document.getElementsByClassName('zero')[0];
 }
 
 function isGameEnded(){
-    if (fieldModel==[[1,2,3,4],[5,6,7,8], [9,10,11,12], [13,14,15,0]]){
-        alert('You won!')
+    for (let i=0; i<fieldModel.length; i++){
+        for(let j=0; j<fieldModel[i].length; j++){
+            if (fieldModel[j][i]!=final[j][i]){return false}
+        }
     }
-}
+    
+        alert('You won!')
+    return true
+    }
+    
+
+ function isSolvable(a){
+     let kDisorder = 0;
+      for ( let i = 1; i< a.length-1; i++){
+        for (let j = i-1; j >= 0; j--) {
+            if (a[j] > a[i]) kDisorder++;
+        }}
+      return !(kDisorder % 2); } 
 
 function setPositionOfNumber(elem, x,y){
     elem.style.left = (25*x + '%');
